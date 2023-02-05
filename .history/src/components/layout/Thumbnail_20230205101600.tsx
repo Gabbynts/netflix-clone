aba8c7273cc77034042b4aec87186fdcc4b74b87
Gabbynts <gabriella.natasya2003@gmail.com>
2023-02-05 10:42:24 +0700

    feat: thumbnail

---
+++ .history/src/components/layout/Thumbnail_20230205101600.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Movie } from 'typings'
+
+interface Props{
+  movies: Movie
+}
+
+function Thumbnail({movies}: Props) {
+  return (
+    <div>Thumbnail</div>
+  )
+}
+
+export default Thumbnail
