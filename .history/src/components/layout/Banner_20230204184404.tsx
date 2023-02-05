ff97f4855e449130da639e3c34e746931ee01eed
Gabbynts <gabriella.natasya2003@gmail.com>
2023-02-05 15:50:30 +0700

    feat: change config value

--- .history/src/components/layout/Banner_20230204184404.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Banner(netflixOriginals) {
-  return (
-    <div></div>
-  )
-}
-
-export default Banner
--- .history/src/components/layout/Banner_20230204184436.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Banner({netflixOriginals}: Props) {
-  return (
-    <div></div>
-  )
-}
-
-export default Banner
